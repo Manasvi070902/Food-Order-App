b185d14d0e30ef2a86ef303871649e556ce71bd7
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-05-14 13:01:11 +0530

    added meals component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 
 import mealsImage from "../../assets/meals.jpeg"
-
+import HeaderCartButton from "./HeaderCardButton"
 import classes from "./Header.module.css"
+
 const Header = props => {
     return <>
     <header className={classes.header}>
-        <h1>React Meals</h1>
-        <button>Cart</button>
+        <h1>Meals</h1>
+        <HeaderCartButton />
     </header>
 
     <div className={classes['main-image']}>
